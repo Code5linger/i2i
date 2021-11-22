6c2f7e9e5b289dad774f9a98922e8420e235218e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-11-22 11:06:57 +0600

    Class 3

--- phonebook/models.py
+++ phonebook/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Phonebook (models.Model):
     name = models.CharField(max_length=255)
     phone = models.TextField
+
+    def __str__(self):
+        return self.name + '===' + self.phone
