d37d5e688978f2dcb4657335b41c9414a076e2ca
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-12-27 22:23:01 +0600

    Use directive to print all the numbers of the phone book

--- class2.3/src/components/HelloWorld.vue
+++ class2.3/src/components/HelloWorld.vue
@@ -1,12 +1,54 @@
 <template>
   <div>
+    <!--Use directive to print all numbers in an array-->
+
     <!--
     <div v-for="x in array" :key="x">{{ x }}</div>
     -->
     <!--
     <div v-for="x in array" :key="x">{{ x }}</div>
     -->
-    
+    <!--
+    <div v-for="x in array" :key="x">{{x}}</div>
+    -->
+    <!--
+    <h1 v-for="x in array" :key="x">{{x}}</h1>
+    -->
+    <!--
+    <h1 v-for="numbers in array" :key="numbers">{{ numbers }}</h1>
+    -->
+
+    <!--Use directive to print all the names of the phone book-->
+    <!-- 
+    <h1 v-for="names in phone_book.name" :key="names">{{names}}</h1>
+    -->
+    <!-- 
+    <h1 v-for="names in phone_book" :key="names">
+      {{ names.name }}
+    </h1>
+    -->
+    <!-- 
+    <h1 v-for="names in phone_book" :key="names" >
+      {{ names.name }}
+    </h1>
+    -->
+    <!-- 
+    <h1 v-for="names in phone_book" :key="names">
+      {{ names.name }}
+    </h1>
+    -->
+
+    <!--Use directive to print all the numbers of the phone book -->
+    <!--
+    <h1 v-for="numbers in phone_book" :key="numbers">
+      {{numbers.number}}
+    </h1>
+    -->
+    <!--
+    <h1 v-for="numbers in phone_book" :key="numbers">
+      {{ numbers.number }}
+    </h1>
+    -->
   </div>
   
 </template>
@@ -18,7 +60,14 @@ export default {
       test: 'test',
       num: '1,2,3,4',
       array: [
-        1,2,3,4,5,6
+        1,2,3,4,5,6,7,8,9
+      ],
+      phone_book: [
+        { name: 'Code', number: '001'},
+        { name: 'Slinger', number: '002'},
+        { name: 'Ethereal', number: '003'},
+        { name: 'Bad', number: '004'},
+        { name: 'Attitude', number: '005'}
       ]
     }
   }
