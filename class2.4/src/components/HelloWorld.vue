92eac5160bc0e96a6444f7469a91ea398a73484e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-12-28 03:01:12 +0600

    On click hide & show the div

--- class2.4/src/components/HelloWorld.vue
+++ class2.4/src/components/HelloWorld.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <button @click="switch_">Click</button>
+    {{ show }}
+    <div v-show="show">
+    <h1>Hello!</h1>
+    
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+      value: 'Testing!',
+      string: 'CodeSlinger',
+      num: 17,
+      array: [
+        1,2,3,4,5,6,7,8,9,0
+      ],
+      matrix: [
+        { name:"person_A", number:"001"},
+        { name:"person_B", number:"002"},
+        { name:"person_C", number:"003"},
+        { name:"person_D", number:"004"},
+      ],
+      show:0
+    }
+  },
+  methods: {
+    switch_() {
+      this.show = !this.show
+    }
+  }
 }
 </script>
 
