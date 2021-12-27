ce1457df755f3f635844086d45b968d5f55d34f7
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-12-27 18:35:51 +0600

    Practicing Directives

--- class2.3/src/components/HelloWorld.vue
+++ class2.3/src/components/HelloWorld.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <h1>Hello World!</h1>
-    {{ test }}
+    <!--
+    <div v-for="x in array" :key="x">{{ x }}</div>
+    -->
+    <!--
+    <div v-for="x in array" :key="x">{{ x }}</div>
+    -->
+    
   </div>
+  
 </template>
 
 <script>
 export default {
   data() {
     return {
-      test: 'test'
+      test: 'test',
+      num: '1,2,3,4',
+      array: [
+        1,2,3,4,5,6
+      ]
     }
   }
 }
