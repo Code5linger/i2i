047172e3eb2efffa2df4bd1d965cda56843a35ca
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-12-29 21:51:51 +0600

    Using directives, change colors of certain names and numbers

--- class2.5/src/components/HelloWorld.vue
+++ class2.5/src/components/HelloWorld.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <h1 v-for="all in array" :key="all">
-      {{ all }}
+    <h1 v-for="all_name in phone_book" :key="all_name">
+      <p v-if="all_name.name == 'Code'" style="color:red">
+        {{ all_name.name }}
+      </p>
+      <p v-else style="color: limegreen">
+        {{ all_name.name }}
+      </p>
     </h1>
-    h1
   </div>
 </template>
 
