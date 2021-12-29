f7d175bab7ccf1f7f0926e7208c7e8301a5cd719
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-12-29 21:47:40 +0600

    Create a matrix with the name and phone number

---
+++ class2.5/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 v-for="all in array" :key="all">
+      {{ all }}
+    </h1>
+    h1
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: "abc",
+      num: 17,
+      array: [
+        1,2,3,4,"a","b","C","D"
+      ],
+      phone_book: [
+        { name: 'Code', number: '001'},
+        { name: 'AA', number: '002'},
+        { name: 'BB', number: '003'},
+        { name: 'CC', number: '004'},
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
